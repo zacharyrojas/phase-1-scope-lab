d4ce60ff33451ab069661f3fbed3de53a567ed50
Zachary Rojas <ztr2194@gmail.com>
2023-03-21 19:17:10 -0500

    scope lab complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    return customerName = 'BOB';
+}
+
+function setBestCustomer() {
+    return bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'jim';
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = 'sam';
+}
